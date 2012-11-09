bc3c26a1ef73d64d9a7835a1cd3d3dcd4f76dffd
Blake Eggleston <blake@blake-desktop.(none)>
2012-11-09 07:17:08 -0800

    adding example cql and crud method stubs

--- cassandraengine/connection.py
+++ cassandraengine/connection.py
@@ -1,5 +1,6 @@
 #http://pypi.python.org/pypi/cql/1.0.4
 #http://code.google.com/a/apache-extras.org/p/cassandra-dbapi2/
+#http://cassandra.apache.org/doc/cql/CQL.html
 
 import cql
 
@@ -7,13 +8,60 @@ _keyspace = 'cse_test'
 _col_fam = 'colfam'
 
 conn = cql.connect('127.0.0.1', 9160)
-cur = conn.cursor()
 
 #cli examples here:
 #http://wiki.apache.org/cassandra/CassandraCli
-cur.execute('create keyspace {};'.format(_keyspace))
+try:
+    conn.set_initial_keyspace(_keyspace)
+except cql.ProgrammingError:
+    #http://www.datastax.com/docs/1.0/references/cql/CREATE_KEYSPACE
+    cur = conn.cursor()
+    cur.execute("create keyspace {} with strategy_class = 'SimpleStrategy' and strategy_options:replication_factor=1;".format(_keyspace))
+    conn.set_initial_keyspace(_keyspace)
+cur = conn.cursor()
 
+#http://www.datastax.com/docs/1.0/dml/using_cql
 #http://stackoverflow.com/questions/10871595/how-do-you-create-a-counter-columnfamily-with-cql3-in-cassandra
-cur.execute('create column family {};'.format(_col_fam))
+try:
+    cur.execute("""create table colfam (id int PRIMARY KEY);""")
+except cql.ProgrammingError:
+    #cur.execute('drop table colfam;')
+    pass
 
-cur.execute('insert into colfam (obj_id, content) values (:obj_id, :content)', dict(obj_id=str(uuid.uuid4()), content='yo!'))
+#http://www.datastax.com/docs/1.0/references/cql/INSERT
+#updates/inserts do the same thing
+cur.execute('insert into colfam (id, content) values (:id, :content)', dict(id=1, content='yo!'))
+
+cur.execute('select * from colfam WHERE id=1;')
+cur.fetchone()
+
+cur.execute("update colfam set content='hey' where id=1;")
+cur.execute('select * from colfam WHERE id=1;')
+cur.fetchone()
+
+cur.execute('delete from colfam WHERE id=1;')
+cur.execute('delete from colfam WHERE id in (1);')
+
+#TODO: Add alter altering existing schema functionality
+#http://www.datastax.com/docs/1.0/references/cql/ALTER_COLUMNFAMILY
+
+def create_column_family(name):
+    """
+    """
+
+def select_row(column_family, **kwargs):
+    """
+    """
+
+def create_row(column_family, **kwargs):
+    """
+    """
+
+def update_row(column_family, **kwargs):
+    """
+    """
+
+def delete_row(column_family, **kwargs):
+    """
+    """
+
