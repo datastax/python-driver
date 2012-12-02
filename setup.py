7afe9300ff37c788536434fd966c274e6611700f
Blake Eggleston <blake@blake-desktop.(none)>
2012-12-02 11:02:09 -0800

    updating version#

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 #python setup.py register
 #python setup.py sdist upload
 
-version = '0.0.3-ALPHA'
+version = '0.0.4-ALPHA'
 
 long_desc = """
 cqlengine is a Cassandra CQL ORM for Python in the style of the Django orm and mongoengine
