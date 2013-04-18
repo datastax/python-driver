40d292c763cd247997c9648aec10b8f9afde7012
Tyler Hobbs <tyler@datastax.com>
2013-04-18 18:50:56 -0500

    Full tests for wait_for_schema_agreement

--- tests/test_control_connection.py
+++ tests/test_control_connection.py
@@ -5,13 +5,22 @@ from cassandra.cluster import _ControlConnection, MAX_SCHEMA_AGREEMENT_WAIT
 from cassandra.pool import Host
 from cassandra.policies import SimpleConvictionPolicy
 
+PEER_IP = "foobar"
+
 class MockMetadata(object):
 
+    def __init__(self):
+        self.hosts = {
+            "ip1": Host("ip1", SimpleConvictionPolicy),
+            "ip2": Host("ip2", SimpleConvictionPolicy)
+        }
+
     def get_host(self, rpc_address):
-        return Host(rpc_address, SimpleConvictionPolicy)
+        return self.hosts.get(rpc_address)
 
     def all_hosts(self):
-        return ['ip1', 'ip2']
+        return self.hosts.values()
+
 
 class MockCluster(object):
 
@@ -32,7 +41,7 @@ class MockConnection(object):
         local_results = [{"schema_version": self.local_schema}]
         local_response = ResultMessage(kind=ResultMessage.KIND_ROWS, results=local_results)
 
-        peer_results = [{"rpc_address": ip, "schema_version": schema_ver}
+        peer_results = [{"rpc_address": ip, "schema_version": schema_ver, "peer": PEER_IP}
                         for ip, schema_ver in self.remote_schemas.iteritems()]
         peer_response = ResultMessage(kind=ResultMessage.KIND_ROWS, results=peer_results)
 
@@ -63,9 +72,50 @@ class ControlConnectionTest(unittest.TestCase):
         self.control_connection._time = self.time
 
     def test_wait_for_schema_agreement(self):
+        """
+        Basic test with all schema versions agreeing
+        """
         self.assertTrue(self.control_connection.wait_for_schema_agreement())
+        # the control connection should not have slept at all
+        self.assertEqual(self.time.clock, 0)
 
     def test_wait_for_schema_agreement_fails(self):
+        """
+        Make sure the control connection sleeps and retries
+        """
         self.connection.remote_schemas['ip2'] = 'b'
+        self.assertFalse(self.control_connection.wait_for_schema_agreement())
+        # the control connection should have slept until it hit the limit
+        self.assertGreaterEqual(self.time.clock, MAX_SCHEMA_AGREEMENT_WAIT)
+
+    def test_wait_for_schema_agreement_skipping(self):
+        """
+        If rpc_address or schema_version isn't set, the host should be skipped
+        """
+        self.connection.remote_schemas['ip3'] = None
+        self.connection.remote_schemas[None] = 'b'
+
+        self.connection.remote_schemas['ip1'] = 'c'
+        self.cluster.metadata.get_host('ip1').monitor.is_up = False
+
+        self.assertTrue(self.control_connection.wait_for_schema_agreement())
+        self.assertEqual(self.time.clock, 0)
+
+    def test_wait_for_schema_agreement_rpc_lookup(self):
+        """
+        If the rpc_address is 0.0.0.0, the "peer" column should be used instead.
+        """
+        self.connection.remote_schemas["0.0.0.0"] = "b"
+        host = Host("0.0.0.0", SimpleConvictionPolicy)
+        self.cluster.metadata.hosts[PEER_IP] = host
+        host.monitor.is_up = False
+
+        # even though the new host has a different schema version, it's
+        # marked as down, so the control connection shouldn't care
+        self.assertTrue(self.control_connection.wait_for_schema_agreement())
+        self.assertEqual(self.time.clock, 0)
+
+        # but once we mark it up, the control connection will care
+        host.monitor.is_up = True
         self.assertFalse(self.control_connection.wait_for_schema_agreement())
         self.assertGreaterEqual(self.time.clock, MAX_SCHEMA_AGREEMENT_WAIT)
