9d0baa8d3b42a3551bfd1244ae9d4b3c2984d06b
Tyler Hobbs <tyler@datastax.com>
2013-04-19 16:43:14 -0500

    More general setup for control connection tests

--- tests/test_control_connection.py
+++ tests/test_control_connection.py
@@ -11,8 +11,8 @@ class MockMetadata(object):
 
     def __init__(self):
         self.hosts = {
-            "ip1": Host("ip1", SimpleConvictionPolicy),
-            "ip2": Host("ip2", SimpleConvictionPolicy)
+            "192.168.1.1": Host("192.168.1.1", SimpleConvictionPolicy),
+            "192.168.1.2": Host("192.168.1.2", SimpleConvictionPolicy)
         }
 
     def get_host(self, rpc_address):
@@ -31,20 +31,41 @@ class MockCluster(object):
 class MockConnection(object):
 
     def __init__(self):
-        self.local_schema = "a"
-        self.remote_schemas = {
-            "ip1": "a",
-            "ip2": "a"
-        }
+        self.local_results = [
+            {
+                "schema_version": "a",
+                "cluster_name": "foocluster",
+                "data_center": "dc1",
+                "rack": "rack1",
+                "partitioner": "Murmur3Partitioner",
+                "tokens": [0, 100, 200]
+            }
+        ]
+
+        self.peer_results = [
+            {
+                "rpc_address": "192.168.1.1",
+                "peer": "10.0.0.1",
+                "schema_version": "a",
+                "data_center": "dc1",
+                "rack": "rack1",
+                "tokens": [1, 101, 201]
+            },
+            {
+                "rpc_address": "192.168.1.2",
+                "peer": "10.0.0.2",
+                "schema_version": "a",
+                "data_center": "dc1",
+                "rack": "rack1",
+                "tokens": [1, 101, 201]
+            }
+        ]
 
     def wait_for_responses(self, peer_query, local_query):
-        local_results = [{"schema_version": self.local_schema}]
-        local_response = ResultMessage(kind=ResultMessage.KIND_ROWS, results=local_results)
-
-        peer_results = [{"rpc_address": ip, "schema_version": schema_ver, "peer": PEER_IP}
-                        for ip, schema_ver in self.remote_schemas.iteritems()]
-        peer_response = ResultMessage(kind=ResultMessage.KIND_ROWS, results=peer_results)
-
+        local_response = ResultMessage(
+            kind=ResultMessage.KIND_ROWS, results=self.local_results)
+        peer_response = ResultMessage(
+            kind=ResultMessage.KIND_ROWS, results=self.peer_results)
         return (peer_response, local_response)
 
 
@@ -83,7 +104,7 @@ class ControlConnectionTest(unittest.TestCase):
         """
         Make sure the control connection sleeps and retries
         """
-        self.connection.remote_schemas['ip2'] = 'b'
+        self.connection.peer_results[1]["schema_version"] = 'b'
         self.assertFalse(self.control_connection.wait_for_schema_agreement())
         # the control connection should have slept until it hit the limit
         self.assertGreaterEqual(self.time.clock, MAX_SCHEMA_AGREEMENT_WAIT)
@@ -92,11 +113,17 @@ class ControlConnectionTest(unittest.TestCase):
         """
         If rpc_address or schema_version isn't set, the host should be skipped
         """
-        self.connection.remote_schemas['ip3'] = None
-        self.connection.remote_schemas[None] = 'b'
+        self.connection.peer_results.append({
+            "rpc_address": "192.168.1.3",
+            "schema_version": None
+        })
+        self.connection.peer_results.append({
+            "rpc_address": None,
+            "schema_version": "b"
+        })
 
-        self.connection.remote_schemas['ip1'] = 'c'
-        self.cluster.metadata.get_host('ip1').monitor.is_up = False
+        self.connection.peer_results[0]['schema_version'] = 'c'
+        self.cluster.metadata.get_host('192.168.1.1').monitor.is_up = False
 
         self.assertTrue(self.control_connection.wait_for_schema_agreement())
         self.assertEqual(self.time.clock, 0)
@@ -105,7 +132,11 @@ class ControlConnectionTest(unittest.TestCase):
         """
         If the rpc_address is 0.0.0.0, the "peer" column should be used instead.
         """
-        self.connection.remote_schemas["0.0.0.0"] = "b"
+        self.connection.peer_results.append({
+            "rpc_address": "0.0.0.0",
+            "schema_version": "b",
+            "peer": PEER_IP
+        })
         host = Host("0.0.0.0", SimpleConvictionPolicy)
         self.cluster.metadata.hosts[PEER_IP] = host
         host.monitor.is_up = False
