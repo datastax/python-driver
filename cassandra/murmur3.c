b0e32005d82f064a7ed026a325d04c8fadba8200
Tyler Hobbs <tyler@datastax.com>
2013-06-14 16:43:11 -0500

    Fix potential compile error in murmur3.c

--- cassandra/murmur3.c
+++ cassandra/murmur3.c
@@ -34,7 +34,7 @@ typedef unsigned __int64 uint64_t;
 
 #include <stdint.h>
 
-#define	FORCE_INLINE __attribute__((always_inline))
+#define	FORCE_INLINE inline __attribute__((always_inline))
 
 inline uint32_t rotl32 ( uint32_t x, int8_t r )
 {
