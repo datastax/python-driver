474bfb4c098a6240edf01c917a529a3cc3ca0e34
Blake Eggleston <bdeggleston@gmail.com>
2013-06-18 11:51:21 -0700

    version number bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 #python setup.py register
 #python setup.py sdist upload
 
-version = '0.3.3'
+version = '0.4'
 
 long_desc = """
 Cassandra CQL 3 Object Mapper for Python
