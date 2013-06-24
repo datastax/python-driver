8ca9c3c88a5fbf16fe6c6b9026efeb2a8ebfab30
Tyler Hobbs <tyler@datastax.com>
2013-06-24 13:39:39 -0500

    PreparedStatement class and from_message factory

--- cassandra/query.py
+++ cassandra/query.py
@@ -34,11 +34,52 @@ class SimpleStatement(Query):
         return self._query_string
 
 
-class PreparedStatement(Query):
+class PreparedStatement(object):
+
+    column_metadata = None
+    query_id = None
+    query = None
+    keyspace = None
+
+    routing_key = None
+    routing_key_indexes = None
+
+    consistency_level = None
+
+    def __init__(self, column_metadata, query_id, routing_key_indexes, query, keyspace):
+        self.column_metadata = column_metadata
+        self.query_id = query_id
+        self.routing_key_indexes = routing_key_indexes
+        self.query = query
+        self.keyspace = keyspace
 
     @classmethod
-    def from_message(cls, col_specs, metadata, query, keyspace):
-        pass
+    def from_message(cls, query_id, column_metadata, cluster_metadata, query, keyspace):
+        if not column_metadata:
+            return PreparedStatement(column_metadata, query_id, None, query, keyspace)
+
+        partition_key_columns = None
+        routing_key_indexes = None
+
+        ks_name, table_name, _, _ = column_metadata[0]
+        ks_meta = cluster_metadata.keyspaces.get(ks_name)
+        if ks_meta:
+            table_meta = ks_meta.tables.get(table_name)
+            if table_meta:
+                partition_key_columns = table_meta.partition_key
+
+                # make a map of {column_name: index} for each column in the statement
+                statement_indexes = dict((c[2], i) for i, c in enumerate(column_metadata))
+
+                # a list of which indexes in the statement correspond to partition key items
+                try:
+                    routing_key_indexes = [statement_indexes[c.name]
+                                           for c in partition_key_columns]
+                except KeyError:
+                    pass  # we're missing a partition key component in the prepared
+                          # statement; just leave routing_key_indexes as None
+
+        return PreparedStatement(column_metadata, query_id, routing_key_indexes, query, keyspace)
 
 
 class ColumnCollection(object):
