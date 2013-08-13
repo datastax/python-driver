faded6f9ae9345d9646d30e2a3bc078e8dd42f32
Tyler Hobbs <tyler@datastax.com>
2013-08-13 14:25:20 -0500

    Documentation for metrics attributes

--- cassandra/metrics.py
+++ cassandra/metrics.py
@@ -7,7 +7,68 @@ log = logging.getLogger(__name__)
 
 class Metrics(object):
 
-    _cluster_proxy = None
+    connection_errors = None
+    """
+    A :class:`greplin.scales.IntStat` count of the number of times that a
+    request to a Cassandra node has failed due to a connection problem.
+    """
+
+    write_timeouts = None
+    """
+    A :class:`greplin.scales.IntStat` count of write requests that resulted
+    in a timeout.
+    """
+
+    read_timeouts = None
+    """
+    A :class:`greplin.scales.IntStat` count of read requests that resulted
+    in a timeout.
+    """
+
+    unavailables = None
+    """
+    A :class:`greplin.scales.IntStat` count of write or read requests that
+    failed due to an insufficient number of replicas being alive to meet
+    the requested :class:`.ConsistencyLevel`.
+    """
+
+    other_errors = None
+    """
+    A :class:`greplin.scales.IntStat` count of all other request failures,
+    including failures caused by invalid requests, bootstrapping nodes,
+    overloaded nodes, etc.
+    """
+
+    retries = None
+    """
+    A :class:`greplin.scales.IntStat` count of the number of times a
+    request was retried based on the :class:`.RetryPolicy` decision.
+    """
+
+    ignores = None
+    """
+    A :class:`greplin.scales.IntStat` count of the number of times a
+    failed request was ignored based on the :class:`.RetryPolicy` decision.
+    """
+
+    known_hosts = None
+    """
+    A :class:`greplin.scales.IntStat` count of the number of nodes in
+    the cluster that the driver is aware of, regardless of whether any
+    connections are opened to those nodes.
+    """
+
+    connected_to = None
+    """
+    A :class:`greplin.scales.IntStat` count of the number of nodes that
+    the driver currently has at least one connection open to.
+    """
+
+    open_connections = None
+    """
+    A :class:`greplin.scales.IntStat` count of the number connections
+    the driver currently has open.
+    """
 
     def __init__(self, cluster_proxy):
         log.debug("Starting metric capture")
@@ -28,3 +89,14 @@ class Metrics(object):
                 lambda: len(set(chain.from_iterable(s._pools.keys() for s in cluster_proxy.sessions)))),
             scales.Stat('open_connections',
                 lambda: sum(sum(p.open_count for p in s._pools.values()) for s in cluster_proxy.sessions)))
+
+        self.connection_errors = self.stats.connection_errors
+        self.write_timeouts = self.stats.write_timeouts
+        self.read_timeouts = self.stats.read_timeouts
+        self.unavailables = self.stats.unavailables
+        self.other_errors = self.stats.other_errors
+        self.retries = self.stats.retries
+        self.ignores = self.stats.ignores
+        self.known_hosts = self.stats.known_hosts
+        self.connected_to = self.stats.connected_to
+        self.open_connections = self.stats.open_connections
