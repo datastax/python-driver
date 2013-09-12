7e5629033dedcaccb89eeed4bc814a30d7afb68d
Tommaso Barbugli <tbarbugli@gmail.com>
2013-09-12 12:33:58 +0200

    consistency_level for execute and execute_async

--- cassandra/cluster.py
+++ cassandra/cluster.py
@@ -580,7 +580,7 @@ class Session(object):
         for host in hosts:
             self.add_host(host)
 
-    def execute(self, query, parameters=None, trace=False):
+    def execute(self, query, parameters=None, trace=False, consistency_level=None):
         """
         Execute the given query and synchronously wait for the response.
 
@@ -607,7 +607,7 @@ class Session(object):
                 "The query argument must be an instance of a subclass of "
                 "cassandra.query.Query when trace=True")
 
-        future = self.execute_async(query, parameters, trace)
+        future = self.execute_async(query, parameters, trace, consistency_level)
         try:
             result = future.result()
         finally:
@@ -619,7 +619,7 @@ class Session(object):
 
         return result
 
-    def execute_async(self, query, parameters=None, trace=False):
+    def execute_async(self, query, parameters=None, trace=False, consistency_level=None):
         """
         Execute the given query and return a :class:`~.ResponseFuture` object
         which callbacks may be attached to for asynchronous response
@@ -656,21 +656,25 @@ class Session(object):
             ...     log.exception("Operation failed:")
 
         """
+
         if isinstance(query, basestring):
             query = SimpleStatement(query)
         elif isinstance(query, PreparedStatement):
             query = query.bind(parameters)
 
+        if consistency_level is None:
+            consistency_level = query.consistency_level
+
         if isinstance(query, BoundStatement):
             message = ExecuteMessage(
                 query_id=query.prepared_statement.query_id,
                 query_params=query.values,
-                consistency_level=query.consistency_level)
+                consistency_level=consistency_level)
         else:
             query_string = query.query_string
             if parameters:
                 query_string = bind_params(query.query_string, parameters)
-            message = QueryMessage(query=query_string, consistency_level=query.consistency_level)
+            message = QueryMessage(query=query_string, consistency_level=consistency_level)
 
         if trace:
             message.tracing = True
