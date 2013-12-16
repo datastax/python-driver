ca708d8aff2c630276cc963cc3daa2d2d650b89b
Joaquin Casares <joaquin@datastax.com>
2013-12-16 13:39:59 -0600

    Add the rest of the CL tests.

--- tests/integration/long/test_consistency.py
+++ tests/integration/long/test_consistency.py
@@ -2,7 +2,8 @@ import cassandra
 
 from cassandra import ConsistencyLevel
 from cassandra.cluster import Cluster
-from cassandra.policies import TokenAwarePolicy, RoundRobinPolicy
+from cassandra.policies import TokenAwarePolicy, RoundRobinPolicy, \
+    DowngradingConsistencyRetryPolicy
 from tests.integration.long.utils import reset_coordinators, force_stop, \
     create_schema, init, query, assert_queried, wait_for_down, wait_for_up, \
     start, get_queried
@@ -384,3 +385,274 @@ class ConsistencyTests(unittest.TestCase):
                 if not cl in [ConsistencyLevel.LOCAL_QUORUM,
                               ConsistencyLevel.EACH_QUORUM]:
                     self._cl_failure(cl, e)
+
+
+    def test_rfone_downgradingcl(self):
+        keyspace = 'test_rfone_downgradingcl'
+        cluster = Cluster(
+            load_balancing_policy=TokenAwarePolicy(RoundRobinPolicy()),
+            default_retry_policy=DowngradingConsistencyRetryPolicy())
+        session = cluster.connect()
+
+        create_schema(session, keyspace, replication_factor=1)
+        init(session, keyspace, 12)
+        query(session, keyspace, 12)
+
+        assert_queried(1, 0)
+        assert_queried(2, 12)
+        assert_queried(3, 0)
+
+        try:
+            reset_coordinators()
+            force_stop(2)
+            wait_for_down(cluster, 2)
+
+            accepted_list = [
+                ConsistencyLevel.ANY
+            ]
+
+            fail_list = [
+                ConsistencyLevel.ONE,
+                ConsistencyLevel.TWO,
+                ConsistencyLevel.QUORUM,
+                ConsistencyLevel.THREE,
+                ConsistencyLevel.ALL,
+                ConsistencyLevel.LOCAL_QUORUM,
+                ConsistencyLevel.EACH_QUORUM
+            ]
+
+            # Test writes that expected to complete successfully
+            for cl in accepted_list:
+                try:
+                    init(session, keyspace, 12, consistency_level=cl)
+                except Exception as e:
+                    self._cl_failure(cl, e)
+
+            # Test reads that expected to complete successfully
+            for cl in accepted_list:
+                try:
+                    reset_coordinators()
+                    query(session, keyspace, 12, consistency_level=cl)
+                    assert_queried(1, 0)
+                    assert_queried(2, 0)
+                    assert_queried(3, 12)
+                except cassandra.InvalidRequest as e:
+                    if not cl in [ConsistencyLevel.ANY]:
+                        self._cl_failure(cl, e)
+                except Exception as e:
+                    self._cl_failure(cl, e)
+
+            # Test writes that expected to fail
+            for cl in fail_list:
+                try:
+                    init(session, keyspace, 12, consistency_level=cl)
+                    self._cl_expected_failure(cl)
+                except cassandra.Unavailable as e:
+                    if not cl in [ConsistencyLevel.ONE,
+                                  ConsistencyLevel.TWO,
+                                  ConsistencyLevel.QUORUM,
+                                  ConsistencyLevel.THREE,
+                                  ConsistencyLevel.ALL]:
+                        self._cl_failure(cl, e)
+                except cassandra.InvalidRequest as e:
+                    if not cl in [ConsistencyLevel.LOCAL_QUORUM,
+                                  ConsistencyLevel.EACH_QUORUM]:
+                        self._cl_failure(cl, e)
+
+            # Test reads that expected to fail
+            for cl in fail_list:
+                try:
+                    query(session, keyspace, 12, consistency_level=cl)
+                    self._cl_expected_failure(cl)
+                except cassandra.Unavailable as e:
+                    if not cl in [ConsistencyLevel.ONE,
+                                  ConsistencyLevel.TWO,
+                                  ConsistencyLevel.QUORUM,
+                                  ConsistencyLevel.THREE,
+                                  ConsistencyLevel.ALL]:
+                        self._cl_failure(cl, e)
+                except cassandra.InvalidRequest as e:
+                    if not cl in [ConsistencyLevel.LOCAL_QUORUM,
+                                  ConsistencyLevel.EACH_QUORUM]:
+                        self._cl_failure(cl, e)
+        finally:
+            start(2)
+            wait_for_up(cluster, 2)
+
+
+    def test_rftwo_downgradingcl(self):
+        keyspace = 'test_rftwo_downgradingcl'
+        cluster = Cluster(
+            load_balancing_policy=TokenAwarePolicy(RoundRobinPolicy()),
+            default_retry_policy=DowngradingConsistencyRetryPolicy())
+        session = cluster.connect()
+
+        create_schema(session, keyspace, replication_factor=2)
+        init(session, keyspace, 12)
+        query(session, keyspace, 12)
+
+        assert_queried(1, 0)
+        assert_queried(2, 12)
+        assert_queried(3, 0)
+
+        try:
+            reset_coordinators()
+            force_stop(2)
+            wait_for_down(cluster, 2)
+
+            accepted_list = [
+                ConsistencyLevel.ANY,
+                ConsistencyLevel.ONE,
+                ConsistencyLevel.TWO,
+                ConsistencyLevel.QUORUM,
+                ConsistencyLevel.THREE,
+                ConsistencyLevel.ALL
+            ]
+
+            fail_list = [
+                ConsistencyLevel.LOCAL_QUORUM,
+                ConsistencyLevel.EACH_QUORUM
+            ]
+
+            # Test writes that expected to complete successfully
+            for cl in accepted_list:
+                try:
+                    init(session, keyspace, 12, consistency_level=cl)
+                except Exception as e:
+                    self._cl_failure(cl, e)
+
+            # Test reads that expected to complete successfully
+            for cl in accepted_list:
+                try:
+                    reset_coordinators()
+                    query(session, keyspace, 12, consistency_level=cl)
+                    assert_queried(1, 0)
+                    assert_queried(2, 0)
+                    assert_queried(3, 12)
+                except cassandra.InvalidRequest as e:
+                    if not cl in [ConsistencyLevel.ANY]:
+                        self._cl_failure(cl, e)
+                except Exception as e:
+                    self._cl_failure(cl, e)
+
+            # Test writes that expected to fail
+            for cl in fail_list:
+                try:
+                    init(session, keyspace, 12, consistency_level=cl)
+                    self._cl_expected_failure(cl)
+                except cassandra.InvalidRequest as e:
+                    if not cl in [ConsistencyLevel.LOCAL_QUORUM,
+                                  ConsistencyLevel.EACH_QUORUM]:
+                        self._cl_failure(cl, e)
+
+            # Test reads that expected to fail
+            for cl in fail_list:
+                try:
+                    query(session, keyspace, 12, consistency_level=cl)
+                    self._cl_expected_failure(cl)
+                except cassandra.InvalidRequest as e:
+                    if not cl in [ConsistencyLevel.LOCAL_QUORUM,
+                                  ConsistencyLevel.EACH_QUORUM]:
+                        self._cl_failure(cl, e)
+        finally:
+            start(2)
+            wait_for_up(cluster, 2)
+
+
+    def test_rfthree_roundrobin_downgradingcl(self):
+        keyspace = 'test_rfthree_roundrobin_downgradingcl'
+        cluster = Cluster(
+            load_balancing_policy=RoundRobinPolicy(),
+            default_retry_policy=DowngradingConsistencyRetryPolicy())
+        self.rfthree_downgradingcl(cluster, keyspace)
+
+    def test_rfthree_tokenaware_downgradingcl(self):
+        keyspace = 'test_rfthree_tokenaware_downgradingcl'
+        cluster = Cluster(
+            load_balancing_policy=TokenAwarePolicy(RoundRobinPolicy()),
+            default_retry_policy=DowngradingConsistencyRetryPolicy())
+        self.rfthree_downgradingcl(cluster, keyspace)
+
+    def rfthree_downgradingcl(self, cluster, keyspace):
+        session = cluster.connect()
+
+        create_schema(session, keyspace, replication_factor=2)
+        init(session, keyspace, 12)
+        query(session, keyspace, 12)
+
+        try:
+            assert_queried(1, 0)
+            assert_queried(2, 12)
+            assert_queried(3, 0)
+        except:
+            assert_queried(1, 4)
+            assert_queried(2, 4)
+            assert_queried(3, 4)
+
+        try:
+            reset_coordinators()
+            force_stop(2)
+            wait_for_down(cluster, 2)
+
+            accepted_list = [
+                ConsistencyLevel.ANY,
+                ConsistencyLevel.ONE,
+                ConsistencyLevel.TWO,
+                ConsistencyLevel.QUORUM,
+                ConsistencyLevel.THREE,
+                ConsistencyLevel.ALL
+            ]
+
+            fail_list = [
+                ConsistencyLevel.LOCAL_QUORUM,
+                ConsistencyLevel.EACH_QUORUM
+            ]
+
+            # Test writes that expected to complete successfully
+            for cl in accepted_list:
+                try:
+                    init(session, keyspace, 12, consistency_level=cl)
+                except Exception as e:
+                    self._cl_failure(cl, e)
+
+            # Test reads that expected to complete successfully
+            for cl in accepted_list:
+                try:
+                    reset_coordinators()
+                    query(session, keyspace, 12, consistency_level=cl)
+                    # assert_queried(1, 0)
+                    # assert_queried(2, 0)
+                    # assert_queried(3, 12)
+                except cassandra.InvalidRequest as e:
+                    if not cl in [ConsistencyLevel.ANY]:
+                        self._cl_failure(cl, e)
+                except Exception as e:
+                    self._cl_failure(cl, e)
+
+            # Test writes that expected to fail
+            for cl in fail_list:
+                try:
+                    init(session, keyspace, 12, consistency_level=cl)
+                    self._cl_expected_failure(cl)
+                except cassandra.InvalidRequest as e:
+                    if not cl in [ConsistencyLevel.LOCAL_QUORUM,
+                                  ConsistencyLevel.EACH_QUORUM]:
+                        self._cl_failure(cl, e)
+
+            # Test reads that expected to fail
+            for cl in fail_list:
+                try:
+                    query(session, keyspace, 12, consistency_level=cl)
+                    self._cl_expected_failure(cl)
+                except cassandra.InvalidRequest as e:
+                    if not cl in [ConsistencyLevel.LOCAL_QUORUM,
+                                  ConsistencyLevel.EACH_QUORUM]:
+                        self._cl_failure(cl, e)
+        finally:
+            start(2)
+            wait_for_up(cluster, 2)
+
+    # TODO: can't be done in this class since we reuse the ccm cluster
+    #       instead we should create these elsewhere
+    # def test_rfthree_downgradingcl_twodcs(self):
+    # def test_rfthree_downgradingcl_twodcs_dcaware(self):
