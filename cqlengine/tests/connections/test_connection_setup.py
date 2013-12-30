b541d3a420983c4103f82508808189b99bda92ca
Blake Eggleston <bdeggleston@gmail.com>
2013-12-30 14:58:58 -0800

    making connection test imports play nice with nose

--- cqlengine/tests/connections/test_connection_setup.py
+++ cqlengine/tests/connections/test_connection_setup.py
@@ -1,20 +1,22 @@
 from unittest import TestCase
-from mock import MagicMock, patch, Mock
+from mock import patch
 
-from cqlengine.connection import setup, CQLConnectionError, Host
+from cqlengine.connection import setup as setup_connection
+from cqlengine.connection import CQLConnectionError, Host
 
 
 class OperationalErrorLoggingTest(TestCase):
+
     @patch('cqlengine.connection.ConnectionPool', return_value=None, autospec=True)
     def test_setup_hosts(self, PatchedConnectionPool):
         with self.assertRaises(CQLConnectionError):
-            setup(hosts=['localhost:abcd'])
+            setup_connection(hosts=['localhost:abcd'])
             self.assertEqual(len(PatchedConnectionPool.mock_calls), 0)
 
         with self.assertRaises(CQLConnectionError):
-            setup(hosts=['localhost:9160:abcd'])
+            setup_connection(hosts=['localhost:9160:abcd'])
             self.assertEqual(len(PatchedConnectionPool.mock_calls), 0)
 
-        setup(hosts=['localhost:9161', 'remotehost'])
+        setup_connection(hosts=['localhost:9161', 'remotehost'])
         self.assertEqual(len(PatchedConnectionPool.mock_calls), 1)
         self.assertEqual(PatchedConnectionPool.call_args[0][0], [Host('localhost', 9161), Host('remotehost', 9160)])
