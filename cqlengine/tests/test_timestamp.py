1c6660199b3f1b5ab6d054151a39cb3cd8ad73a1
Blake Eggleston <bdeggleston@gmail.com>
2014-03-11 13:08:53 -0700

    reformatting

--- cqlengine/tests/test_timestamp.py
+++ cqlengine/tests/test_timestamp.py
@@ -3,7 +3,6 @@ Tests surrounding the blah.timestamp( timedelta(seconds=30) ) format.
 """
 from datetime import timedelta, datetime
 
-import unittest
 from uuid import uuid4
 import mock
 import sure
@@ -19,18 +18,20 @@ class TestTimestampModel(Model):
 
 
 class BaseTimestampTest(BaseCassEngTestCase):
+
     @classmethod
     def setUpClass(cls):
         super(BaseTimestampTest, cls).setUpClass()
         sync_table(TestTimestampModel)
 
+
 class BatchTest(BaseTimestampTest):
+
     def test_batch_is_included(self):
         with mock.patch.object(ConnectionPool, "execute") as m, BatchQuery(timestamp=timedelta(seconds=30)) as b:
             TestTimestampModel.batch(b).create(count=1)
 
         "USING TIMESTAMP".should.be.within(m.call_args[0][0])
-
 
 
 class CreateWithTimestampTest(BaseTimestampTest):
@@ -43,8 +44,6 @@ class CreateWithTimestampTest(BaseTimestampTest):
 
         query.should.match(r"INSERT.*USING TIMESTAMP")
         query.should_not.match(r"TIMESTAMP.*INSERT")
-
-
 
     def test_timestamp_not_included_on_normal_create(self):
         with mock.patch.object(ConnectionPool, "execute") as m:
@@ -90,7 +89,9 @@ class UpdateWithTimestampTest(BaseTimestampTest):
         query = m.call_args[0][0]
         "USING TIMESTAMP".should.be.within(query)
 
+
 class DeleteWithTimestampTest(BaseTimestampTest):
+
     def test_non_batch(self):
         """
         we don't expect the model to come back at the end because the deletion timestamp should be in the future
@@ -121,10 +122,6 @@ class DeleteWithTimestampTest(BaseTimestampTest):
         tmp.timestamp(timedelta(seconds=5))
         tmp.update()
         tmp._timestamp.shouldnt.be.ok
-
-
-
-
 
     def test_blind_delete(self):
         """
@@ -176,8 +173,5 @@ class DeleteWithTimestampTest(BaseTimestampTest):
         TestTimestampModel.objects(id=uid).timestamp(timedelta(seconds=-60)).delete()
 
         TestTimestampModel.get(id=uid)
-
-
-
 
 
