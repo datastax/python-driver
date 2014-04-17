ea080bd1e7cc2a150466c2ab23fac59da1d7238d
Tyler Hobbs <tyler@datastax.com>
2014-04-16 19:22:37 -0500

    Make version 2.0.0.dev on 2.0 branch

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -23,7 +23,7 @@ class NullHandler(logging.Handler):
 logging.getLogger('cassandra').addHandler(NullHandler())
 
 
-__version_info__ = (1, 0, 2, 'post')
+__version_info__ = (2, 0, 0, 'dev')
 __version__ = '.'.join(map(str, __version_info__))
 
 
