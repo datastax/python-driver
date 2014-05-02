4cfa123da2ccf416e2cb7e4bd9bc0c189a06081b
Tyler Hobbs <tyler@datastax.com>
2014-05-02 18:34:20 -0500

    Add test logger if not added by nose

--- tests/__init__.py
+++ tests/__init__.py
@@ -16,3 +16,8 @@ import logging
 
 log = logging.getLogger()
 log.setLevel('DEBUG')
+# if nose didn't already attach a log handler, add one here
+if not log.handlers:
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s [%(module)s:%(lineno)s]: %(message)s'))
+    log.addHandler(handler)
