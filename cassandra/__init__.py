ceaece0b6343bc1a951d4760980713ab70c4392c
Tyler Hobbs <tyler@datastax.com>
2014-05-28 18:38:15 -0500

    Release 2.0.1

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -23,7 +23,7 @@ class NullHandler(logging.Handler):
 logging.getLogger('cassandra').addHandler(NullHandler())
 
 
-__version_info__ = (2, 0, 0, 'post')
+__version_info__ = (2, 0, 1)
 __version__ = '.'.join(map(str, __version_info__))
 
 
