c80d463b8d4f19909e022233fb3412a3b5c21244
Jon Haddad <jon@jonhaddad.com>
2014-06-19 09:12:49 -0700

    note in test so I know what exactly is supposed to come back

--- cqlengine/tests/model/test_clustering_order.py
+++ cqlengine/tests/model/test_clustering_order.py
@@ -32,4 +32,5 @@ class TestClusteringOrder(BaseCassEngTestCase):
             TestModel.create(id=1, clustering_key=i)
 
         values = list(TestModel.objects.values_list('clustering_key', flat=True))
+        # [19L, 18L, 17L, 16L, 15L, 14L, 13L, 12L, 11L, 10L, 9L, 8L, 7L, 6L, 5L, 4L, 3L, 2L, 1L, 0L]
         self.assertEquals(values, sorted(items, reverse=True))
