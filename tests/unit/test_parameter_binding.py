9a391e7d79aeeb3195b126391caf096a4cf67026
Tyler Hobbs <tyler@datastax.com>
2014-07-22 14:35:22 -0500

    Update parameter binding unit test

--- tests/unit/test_parameter_binding.py
+++ tests/unit/test_parameter_binding.py
@@ -17,7 +17,7 @@ try:
 except ImportError:
     import unittest # noqa
 
-from cassandra.encoder import default_encoder
+from cassandra.encoder import Encoder
 from cassandra.query import bind_params, ValueSequence
 from cassandra.query import PreparedStatement, BoundStatement
 from cassandra.cqltypes import Int32Type
@@ -29,31 +29,31 @@ from six.moves import xrange
 class ParamBindingTest(unittest.TestCase):
 
     def test_bind_sequence(self):
-        result = bind_params("%s %s %s", (1, "a", 2.0), default_encoder)
+        result = bind_params("%s %s %s", (1, "a", 2.0), Encoder())
         self.assertEqual(result, "1 'a' 2.0")
 
     def test_bind_map(self):
-        result = bind_params("%(a)s %(b)s %(c)s", dict(a=1, b="a", c=2.0), default_encoder)
+        result = bind_params("%(a)s %(b)s %(c)s", dict(a=1, b="a", c=2.0), Encoder())
         self.assertEqual(result, "1 'a' 2.0")
 
     def test_sequence_param(self):
-        result = bind_params("%s", (ValueSequence((1, "a", 2.0)),), default_encoder)
+        result = bind_params("%s", (ValueSequence((1, "a", 2.0)),), Encoder())
         self.assertEqual(result, "( 1 , 'a' , 2.0 )")
 
     def test_generator_param(self):
-        result = bind_params("%s", ((i for i in xrange(3)),), default_encoder)
+        result = bind_params("%s", ((i for i in xrange(3)),), Encoder())
         self.assertEqual(result, "[ 0 , 1 , 2 ]")
 
     def test_none_param(self):
-        result = bind_params("%s", (None,), default_encoder)
+        result = bind_params("%s", (None,), Encoder())
         self.assertEqual(result, "NULL")
 
     def test_list_collection(self):
-        result = bind_params("%s", (['a', 'b', 'c'],), default_encoder)
+        result = bind_params("%s", (['a', 'b', 'c'],), Encoder())
         self.assertEqual(result, "[ 'a' , 'b' , 'c' ]")
 
     def test_set_collection(self):
-        result = bind_params("%s", (set(['a', 'b']),), default_encoder)
+        result = bind_params("%s", (set(['a', 'b']),), Encoder())
         self.assertIn(result, ("{ 'a' , 'b' }", "{ 'b' , 'a' }"))
 
     def test_map_collection(self):
@@ -61,11 +61,11 @@ class ParamBindingTest(unittest.TestCase):
         vals['a'] = 'a'
         vals['b'] = 'b'
         vals['c'] = 'c'
-        result = bind_params("%s", (vals,), default_encoder)
+        result = bind_params("%s", (vals,), Encoder())
         self.assertEqual(result, "{ 'a' : 'a' , 'b' : 'b' , 'c' : 'c' }")
 
     def test_quote_escaping(self):
-        result = bind_params("%s", ("""'ef''ef"ef""ef'""",), default_encoder)
+        result = bind_params("%s", ("""'ef''ef"ef""ef'""",), Encoder())
         self.assertEqual(result, """'''ef''''ef"ef""ef'''""")
 
 
