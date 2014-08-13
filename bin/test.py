2ad4808c9749229cc59c9aeaeeee83f72c91985e
Jon Haddad <jon@jonhaddad.com>
2014-08-13 10:50:35 -0700

    moving setup into the bin/test.py

--- bin/test.py
+++ bin/test.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 
-
+import os
 import nose
+import sys
+
+sys.path.append("")
 
 # setup cassandra
+from cqlengine import connection
+
+if os.environ.get('CASSANDRA_TEST_HOST'):
+    CASSANDRA_TEST_HOST = os.environ['CASSANDRA_TEST_HOST']
+else:
+    CASSANDRA_TEST_HOST = 'localhost'
+
+protocol_version = int(os.environ.get("CASSANDRA_PROTOCOL_VERSION", 2))
+
+connection.setup([CASSANDRA_TEST_HOST], protocol_version=protocol_version, default_keyspace='cqlengine_test')
+
+try:
+    c_version = os.environ["CASSANDRA_VERSION"]
+except:
+    print "CASSANDRA_VERSION must be set as an environment variable. One of (12, 20, 21)"
+    raise
 
 nose.main()
--- cqlengine/tests/base.py
+++ cqlengine/tests/base.py
@@ -4,15 +4,6 @@ import os
 from cqlengine.connection import get_session
 
 
-if os.environ.get('CASSANDRA_TEST_HOST'):
-    CASSANDRA_TEST_HOST = os.environ['CASSANDRA_TEST_HOST']
-else:
-    CASSANDRA_TEST_HOST = 'localhost'
-
-protocol_version = int(os.environ.get("CASSANDRA_PROTOCOL_VERSION", 2))
-
-connection.setup([CASSANDRA_TEST_HOST], protocol_version=protocol_version, default_keyspace='cqlengine_test')
-
 class BaseCassEngTestCase(TestCase):
 
     # @classmethod
