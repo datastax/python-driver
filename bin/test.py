503e2d1ecfa04669a70c1d7b84e8bfc5dc18c4f7
Jon Haddad <jon@jonhaddad.com>
2014-08-13 13:22:08 -0700

    fixing protocol version

--- bin/test.py
+++ bin/test.py
@@ -9,19 +9,23 @@ sys.path.append("")
 # setup cassandra
 from cqlengine import connection
 
+try:
+    CASSANDRA_VERSION = os.environ["CASSANDRA_VERSION"]
+except:
+    print "CASSANDRA_VERSION must be set as an environment variable. One of (12, 20, 21)"
+    raise
+
 if os.environ.get('CASSANDRA_TEST_HOST'):
     CASSANDRA_TEST_HOST = os.environ['CASSANDRA_TEST_HOST']
 else:
     CASSANDRA_TEST_HOST = 'localhost'
 
-protocol_version = int(os.environ.get("CASSANDRA_PROTOCOL_VERSION", 2))
+if CASSANDRA_VERSION < 20:
+    protocol_version = 1
+else:
+    protocol_version = 2
 
 connection.setup([CASSANDRA_TEST_HOST], protocol_version=protocol_version, default_keyspace='cqlengine_test')
 
-try:
-    c_version = os.environ["CASSANDRA_VERSION"]
-except:
-    print "CASSANDRA_VERSION must be set as an environment variable. One of (12, 20, 21)"
-    raise
 
 nose.main()
