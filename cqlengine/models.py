6f2d013e0254095d316de6bb59222475977e190c
Jon Haddad <jon@jonhaddad.com>
2014-08-13 15:55:07 -0700

    dealing with itemsview

--- cqlengine/models.py
+++ cqlengine/models.py
@@ -636,7 +636,7 @@ class ModelMetaClass(type):
 
         is_polymorphic_base = any([c[1].polymorphic_key for c in column_definitions])
 
-        column_definitions = inherited_columns.items() + column_definitions
+        column_definitions = [x for x in inherited_columns.items()] + column_definitions
 
         polymorphic_columns = [c for c in column_definitions if c[1].polymorphic_key]
         is_polymorphic = len(polymorphic_columns) > 0
