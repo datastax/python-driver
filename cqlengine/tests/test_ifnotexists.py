fdd04857e5c2616990bd6c5388433a3b8db2875b
mission.liao <missionaryliao@gmail.com>
2014-08-16 22:06:53 +0800

    add test case for batch query

--- cqlengine/tests/test_ifnotexists.py
+++ cqlengine/tests/test_ifnotexists.py
@@ -2,10 +2,11 @@ from unittest import skipUnless
 from cqlengine.management import sync_table, drop_table, create_keyspace, delete_keyspace
 from cqlengine.tests.base import BaseCassEngTestCase
 from cqlengine.models import Model
-from cqlengine import columns
+from cqlengine import columns, BatchQuery
 from uuid import uuid4
+from datetime import datetime
 import mock
-from cqlengine.connection import get_cluster, get_session
+from cqlengine.connection import get_cluster
 
 cluster = get_cluster()
 
@@ -58,7 +59,6 @@ class IfNotExistsInsertTests(BaseIfNotExistsTest):
         self.assertEquals(tm.count, 8)
         self.assertEquals(tm.text, '123456789')
 
-    @skipUnless(cluster.protocol_version >= 2, "only runs against the cql3 protocol v2.0")
     def test_insert_if_not_exists_failure(self):
         """ tests that insertion with if_not_exists failure """
 
@@ -74,15 +74,48 @@ class IfNotExistsInsertTests(BaseIfNotExistsTest):
         self.assertEquals(tm.count, 9)
         self.assertEquals(tm.text, '111111111111')
 
+    @skipUnless(cluster.protocol_version >= 2, "only runs against the cql3 protocol v2.0")
+    def test_batch_insert_if_not_exists_success(self):
+        """ tests that batch insertion with if_not_exists work as expected """
+
+        id = uuid4()
+
+        with BatchQuery() as b:
+            TestIfNotExistsModel.batch(b).if_not_exists().create(id=id, count=8, text='123456789')
+
+        with BatchQuery() as b:
+            TestIfNotExistsModel.batch(b).if_not_exists().create(id=id, count=9, text='111111111111')
+
+        q = TestIfNotExistsModel.objects(id=id)
+        self.assertEqual(len(q), 1)
+
+        tm = q.first()
+        self.assertEquals(tm.count, 8)
+        self.assertEquals(tm.text, '123456789')
+
+    def test_batch_insert_if_not_exists_failure(self):
+        """ tests that batch insertion with if_not_exists failure """
+        id = uuid4()
+
+        with BatchQuery() as b:
+            TestIfNotExistsModel.batch(b).create(id=id, count=8, text='123456789')
+        with BatchQuery() as b:
+            TestIfNotExistsModel.batch(b).create(id=id, count=9, text='111111111111')
+
+        q = TestIfNotExistsModel.objects(id=id)
+        self.assertEquals(len(q), 1)
+
+        tm = q.first()
+        self.assertEquals(tm.count, 9)
+        self.assertEquals(tm.text, '111111111111')
+
 
 class IfNotExistsModelTest(BaseIfNotExistsTest):
 
     def test_if_not_exists_included_on_create(self):
         """ tests that if_not_exists on models works as expected """
 
-        session = get_session()
-
-        with mock.patch.object(session, 'execute') as m:
+        with mock.patch.object(self.session, 'execute') as m:
             TestIfNotExistsModel.if_not_exists().create(count=8)
 
         query = m.call_args[0][0].query_string
@@ -91,9 +124,7 @@ class IfNotExistsModelTest(BaseIfNotExistsTest):
     def test_if_not_exists_included_on_save(self):
         """ tests if we correctly put 'IF NOT EXISTS' for insert statement """
 
-        session = get_session()
-
-        with mock.patch.object(session, 'execute') as m:
+        with mock.patch.object(self.session, 'execute') as m:
             tm = TestIfNotExistsModel(count=8)
             tm.if_not_exists(True).save()
 
@@ -104,6 +135,14 @@ class IfNotExistsModelTest(BaseIfNotExistsTest):
         """ ensure we get a queryset description back """
         qs = TestIfNotExistsModel.if_not_exists()
         self.assertTrue(isinstance(qs, TestIfNotExistsModel.__queryset__), type(qs))
+
+    def test_batch_if_not_exists(self):
+        """ ensure 'IF NOT EXISTS' exists in statement when in batch """ 
+        with mock.patch.object(self.session, 'execute') as m:
+            with BatchQuery() as b:
+                TestIfNotExistsModel.batch(b).if_not_exists().create(count=8)
+
+        self.assertIn("IF NOT EXISTS", m.call_args[0][0].query_string)
 
 
 class IfNotExistsInstanceTest(BaseIfNotExistsTest):
@@ -122,13 +161,11 @@ class IfNotExistsInstanceTest(BaseIfNotExistsTest):
         """
         make sure we don't put 'IF NOT EXIST' in update statements
         """
-        session = get_session()
-
         o = TestIfNotExistsModel.create(text="whatever")
         o.text = "new stuff"
         o = o.if_not_exists(True)
 
-        with mock.patch.object(session, 'execute') as m:
+        with mock.patch.object(self.session, 'execute') as m:
             o.save()
 
         query = m.call_args[0][0].query_string
