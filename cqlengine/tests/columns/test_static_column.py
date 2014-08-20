921c5245231994d280a4c2362ff006524fda4c79
Andy Zhao <andyzhao888@gmail.com>
2014-08-19 22:55:32 -0400

    fix updating static column only exception
    fix updating static column only exception due to extra clustering key
    in where clause

---
+++ cqlengine/tests/columns/test_static_column.py
@@ -0,0 +1,58 @@
+from uuid import uuid4
+
+from cqlengine import Model
+from cqlengine import columns
+from cqlengine.management import sync_table, drop_table
+from cqlengine.models import ModelDefinitionException
+from cqlengine.tests.base import BaseCassEngTestCase
+
+
+class TestStaticModel(Model):
+    __keyspace__ = 'test'
+    partition = columns.UUID(primary_key=True, default=uuid4)
+    cluster = columns.UUID(primary_key=True, default=uuid4)
+    static = columns.Text(static=True)
+    text = columns.Text()
+
+
+class TestStaticColumn(BaseCassEngTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestStaticColumn, cls).setUpClass()
+        drop_table(TestStaticModel)
+        sync_table(TestStaticModel)
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestStaticColumn, cls).tearDownClass()
+        drop_table(TestStaticModel)
+
+    def test_mixed_updates(self):
+        """ Tests that updates on both static and non-static columns work as intended """
+        instance = TestStaticModel.create()
+        instance.static = "it's shared"
+        instance.text = "some text" 
+        instance.save()
+        
+        u = TestStaticModel.get(partition=instance.partition)
+        u.static = "it's still shared"
+        u.text = "another text"
+        u.update()
+        actual = TestStaticModel.get(partition=u.partition)
+        
+        assert actual.static == "it's still shared"
+
+    def test_static_only_updates(self):
+        """ Tests that updates on static only column work as intended """
+        instance = TestStaticModel.create()
+        instance.static = "it's shared"
+        instance.text = "some text" 
+        instance.save()
+        
+        u = TestStaticModel.get(partition=instance.partition)
+        u.static = "it's still shared"
+        u.update()
+        actual = TestStaticModel.get(partition=u.partition)        
+        assert actual.static == "it's still shared"
+
