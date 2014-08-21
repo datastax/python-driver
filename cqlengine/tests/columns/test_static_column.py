d986b0f7e6360ff83cf1a01d928bc10392e9d8cb
andy8zhao <andy@AndyMBP.local>
2014-08-21 16:25:24 -0400

    skip test if protocol version < 2
    Skip static column only update unit test if cassandra protocol version
    < 2

--- cqlengine/tests/columns/test_static_column.py
+++ cqlengine/tests/columns/test_static_column.py
@@ -1,11 +1,13 @@
 from uuid import uuid4
-
+from unittest import skipUnless
 from cqlengine import Model
 from cqlengine import columns
 from cqlengine.management import sync_table, drop_table
 from cqlengine.models import ModelDefinitionException
 from cqlengine.tests.base import BaseCassEngTestCase
+from cqlengine.connection import get_cluster
 
+cluster = get_cluster()
 
 class TestStaticModel(Model):
     __keyspace__ = 'test'
@@ -28,6 +30,7 @@ class TestStaticColumn(BaseCassEngTestCase):
         super(TestStaticColumn, cls).tearDownClass()
         drop_table(TestStaticModel)
 
+    @skipUnless(cluster.protocol_version >= 2, "only runs against the cql3 protocol v2.0")
     def test_mixed_updates(self):
         """ Tests that updates on both static and non-static columns work as intended """
         instance = TestStaticModel.create()
@@ -43,6 +46,7 @@ class TestStaticColumn(BaseCassEngTestCase):
         
         assert actual.static == "it's still shared"
 
+    @skipUnless(cluster.protocol_version >= 2, "only runs against the cql3 protocol v2.0")
     def test_static_only_updates(self):
         """ Tests that updates on static only column work as intended """
         instance = TestStaticModel.create()
