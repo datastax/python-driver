07a4a7c403ce01e7290821af7e2b4439f04f5580
Adam Holmberg <Adam.Holmberg@datastax.com>
2014-11-24 16:49:12 -0600

    Don't try to connect to peers with incomplete meta
    PYTHON-163

--- cassandra/cluster.py
+++ cassandra/cluster.py
@@ -2060,19 +2060,25 @@ class ControlConnection(object):
             if not addr or addr in ["0.0.0.0", "::"]:
                 addr = row.get("peer")
 
-            found_hosts.add(addr)
+            tokens = row.get("tokens")
+            peer_meta_complete = bool(tokens)
 
             host = self._cluster.metadata.get_host(addr)
             datacenter = row.get("data_center")
             rack = row.get("rack")
             if host is None:
                 log.debug("[control connection] Found new host to connect to: %s", addr)
-                host = self._cluster.add_host(addr, datacenter, rack, signal=True)
-                should_rebuild_token_map = True
+                host = self._cluster.add_host(addr, datacenter, rack, signal=peer_meta_complete)
+                should_rebuild_token_map |= peer_meta_complete
             else:
-                should_rebuild_token_map |= self._update_location_info(host, datacenter, rack)
+                should_rebuild_token_map |= self._update_location_info(host, datacenter, rack) and peer_meta_complete
 
-            tokens = row.get("tokens")
+            if not peer_meta_complete:
+              log.warn("Excluding host (%s) with no tokens in system.peers table of %s." % (addr, connection.host))
+              continue
+
+            found_hosts.add(addr)
+
             if partitioner and tokens:
                 token_map[host] = tokens
 
