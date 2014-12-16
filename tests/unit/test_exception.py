ddf0eb9d10dd58de0f2b9f50de7815ce346b3c7b
Irina Kaplounova <irina.kaplounova@datastax.com>
2014-12-16 01:13:17 -0800

    Test for PYTHON-180

---
+++ tests/unit/test_exception.py
@@ -0,0 +1,56 @@
+# Copyright 2014 DataStax, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from cassandra import Unavailable, Timeout, ConsistencyLevel
+import re
+
+
+class ConsistencyExceptionTest(unittest.TestCase):
+    """
+    Verify Cassandra Exception string representation
+    """
+
+    def extract_consistency(self, msg):
+        """
+        Given message that has 'consistency': 'value', extract consistency value as a string
+        :param msg: message with consistency value
+        :return: String representing consistency value
+        """
+        match = re.search("'consistency':\s+'([\w\s]+)'", msg)
+        return match and match.group(1)
+
+    def test_timeout_consistency(self):
+        """
+        Verify that Timeout exception object translates consistency from input value to correct output string
+        """
+        consistency_str = self.extract_consistency(repr(Timeout("Timeout Message", consistency=None)))
+        self.assertEqual(consistency_str, 'Not Set')
+        for c in ConsistencyLevel.value_to_name.keys():
+            consistency_str = self.extract_consistency(repr(Timeout("Timeout Message", consistency=c)))
+        self.assertEqual(consistency_str, ConsistencyLevel.value_to_name[c])
+
+    def test_unavailable_consistency(self):
+        """
+        Verify that Unavailable exception object translates consistency from input value to correct output string
+        """
+        consistency_str = self.extract_consistency(repr(Unavailable("Unavailable Message", consistency=None)))
+        self.assertEqual(consistency_str, 'Not Set')
+        for c in ConsistencyLevel.value_to_name.keys():
+            consistency_str = self.extract_consistency(repr(Unavailable("Timeout Message", consistency=c)))
+        self.assertEqual(consistency_str, ConsistencyLevel.value_to_name[c])
