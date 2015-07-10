1f0c6c312bce36d0892e18725baace3a0ae02a28
Adam Holmberg <Adam.Holmberg@datastax.com>
2015-07-10 09:52:48 -0500

    Limit mock dependency to version 1.0.1
    1.1.0 dropped Python 2.6 support, and at least one call assert function
    currently used by tests.

--- setup.py
+++ setup.py
@@ -216,7 +216,7 @@ def run_setup(extensions):
         keywords='cassandra,cql,orm',
         include_package_data=True,
         install_requires=dependencies,
-        tests_require=['nose', 'mock', 'PyYAML', 'pytz', 'sure'],
+        tests_require=['nose', 'mock<=1.0.1', 'PyYAML', 'pytz', 'sure'],
         classifiers=[
             'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
