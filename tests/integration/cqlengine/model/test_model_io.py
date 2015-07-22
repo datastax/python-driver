17f2bc13c2fda731e9de8217790730b151f962ec
Kevin Deldycke <kdeldycke@ocs.online.net>
2015-07-22 15:10:54 +0200

    Unit-tests CQLengine's previous value tracking.
    Test previous_value of an already-persisted object as well as one that
    was just instanciated.
    
    Refs PYTHON-348.

--- tests/integration/cqlengine/model/test_model_io.py
+++ tests/integration/cqlengine/model/test_model_io.py
@@ -347,6 +347,80 @@ class TestUpdating(BaseCassEngTestCase):
         self.instance.save()
         assert self.instance.get_changed_columns() == []
 
+    def test_previous_value_tracking_of_persisted_instance(self):
+        # Check initial internal states.
+        assert self.instance.get_changed_columns() == []
+        assert self.instance._values['count'].previous_value == 0
+
+        # Change value and check internal states.
+        self.instance.count = 1
+        assert self.instance.get_changed_columns() == ['count']
+        assert self.instance._values['count'].previous_value == 0
+
+        # Internal states should be updated on save.
+        self.instance.save()
+        assert self.instance.get_changed_columns() == []
+        assert self.instance._values['count'].previous_value == 1
+
+        # Change value twice.
+        self.instance.count = 2
+        assert self.instance.get_changed_columns() == ['count']
+        assert self.instance._values['count'].previous_value == 1
+        self.instance.count = 3
+        assert self.instance.get_changed_columns() == ['count']
+        assert self.instance._values['count'].previous_value == 1
+
+        # Internal states updated on save.
+        self.instance.save()
+        assert self.instance.get_changed_columns() == []
+        assert self.instance._values['count'].previous_value == 3
+
+        # Change value and reset it.
+        self.instance.count = 2
+        assert self.instance.get_changed_columns() == ['count']
+        assert self.instance._values['count'].previous_value == 3
+        self.instance.count = 3
+        assert self.instance.get_changed_columns() == []
+        assert self.instance._values['count'].previous_value == 3
+
+        # Nothing to save: values in initial conditions.
+        self.instance.save()
+        assert self.instance.get_changed_columns() == []
+        assert self.instance._values['count'].previous_value == 3
+
+    def test_previous_value_tracking_on_instanciation(self):
+        self.instance = TestMultiKeyModel(
+            partition=random.randint(0, 1000),
+            cluster=random.randint(0, 1000),
+            count=0,
+            text='happy')
+
+        # Columns of instances not persisted yet should be marked as changed.
+        assert set(self.instance.get_changed_columns()) == set([
+            'partition', 'cluster', 'count', 'text'])
+        assert self.instance._values['partition'].previous_value is None
+        assert self.instance._values['cluster'].previous_value is None
+        assert self.instance._values['count'].previous_value is None
+        assert self.instance._values['text'].previous_value is None
+
+        # Value changes doesn't affect internal states.
+        self.instance.count = 1
+        assert 'count' in self.instance.get_changed_columns()
+        assert self.instance._values['count'].previous_value is None
+        self.instance.count = 2
+        assert 'count' in self.instance.get_changed_columns()
+        assert self.instance._values['count'].previous_value is None
+
+        # Value reset is properly tracked.
+        self.instance.count = None
+        assert 'count' not in self.instance.get_changed_columns()
+        assert self.instance._values['count'].previous_value == None
+
+        self.instance.save()
+        assert self.instance.get_changed_columns() == []
+        assert self.instance._values['count'].previous_value == None
+        assert self.instance.count is None
+
 
 class TestCanUpdate(BaseCassEngTestCase):
 
