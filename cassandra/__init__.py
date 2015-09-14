3d04d48c27d200ccdc0faf75262587724c6c5a05
Adam Holmberg <Adam.Holmberg@datastax.com>
2015-09-14 13:54:13 -0500

    post-release version update

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -23,7 +23,7 @@ class NullHandler(logging.Handler):
 logging.getLogger('cassandra').addHandler(NullHandler())
 
 
-__version_info__ = (2, 7, 2)
+__version_info__ = (2, 7, 2, 'post0')
 __version__ = '.'.join(map(str, __version_info__))
 
 
