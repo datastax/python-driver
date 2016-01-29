a694d3b4fe2acfa430be8ad1b031abc4ae438c86
Adam Holmberg <Adam.Holmberg@datastax.com>
2016-01-29 15:08:15 -0600

    [ci skip] 3.1a1 version update

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -23,7 +23,7 @@ class NullHandler(logging.Handler):
 logging.getLogger('cassandra').addHandler(NullHandler())
 
 
-__version_info__ = (3, 0, 0, 'post0')
+__version_info__ = (3, 1, '0a1')
 __version__ = '.'.join(map(str, __version_info__))
 
 
