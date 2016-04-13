298785b3472466b83a9faff4c149609f197492d4
Adam Holmberg <Adam.Holmberg@datastax.com>
2016-04-13 14:33:09 -0500

    3.2.1 patch version and changelog

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -22,7 +22,7 @@ class NullHandler(logging.Handler):
 
 logging.getLogger('cassandra').addHandler(NullHandler())
 
-__version_info__ = (3, 2, 0, 'post0')
+__version_info__ = (3, 2, 1)
 __version__ = '.'.join(map(str, __version_info__))
 
 
