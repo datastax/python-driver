156fcbfd393578638eccf70951d85f34cca97695
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2016-10-04 15:57:34 +0800

    12736: self.prepared_statement may be None for a batch of prepared statements

--- cassandra/cluster.py
+++ cassandra/cluster.py
@@ -3667,7 +3667,8 @@ class ResponseFuture(object):
             if response.kind == RESULT_KIND_PREPARED:
                 # result metadata is the only thing that could have changed from an alter
                 _, _, _, result_metadata = response.results
-                self.prepared_statement.result_metadata = result_metadata
+                if self.prepared_statement:
+                    self.prepared_statement.result_metadata = result_metadata
 
                 # use self._query to re-use the same host and
                 # at the same time properly borrow the connection
