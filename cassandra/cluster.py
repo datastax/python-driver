b2d975841ad6e7413b10335432e7e8fec6cd2a8b
Jim Witschey <jim.witschey@gmail.com>
2017-03-09 12:43:27 -0500

    finish merge from integration-tested branch

--- cassandra/cluster.py
+++ cassandra/cluster.py
@@ -3687,8 +3687,7 @@ class ResponseFuture(object):
             if response.kind == RESULT_KIND_PREPARED:
                 # result metadata is the only thing that could have changed from an alter
                 _, _, _, result_metadata = response.results
-                if self.prepared_statement:
-                    self.prepared_statement.result_metadata = result_metadata
+                self.prepared_statement.result_metadata = result_metadata
 
                 # use self._query to re-use the same host and
                 # at the same time properly borrow the connection
