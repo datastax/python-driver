jatin7/python-dse-driver

remove unused old header_struct

dse/protocol.py
@@ -24,7 +24,7 @@ from dse import (Unavailable, WriteTimeout, ReadTimeout,
                  UnsupportedOperation, UserFunctionDescriptor,
                  UserAggregateDescriptor, SchemaTargetType)
 from dse.marshal import (int32_pack, int32_unpack, uint16_pack, uint16_unpack,
-                         uint8_pack, int8_unpack, uint64_pack, header_pack,
+                         uint8_pack, int8_unpack, uint64_pack,
                          v3_header_pack, uint32_pack)
 from dse.cqltypes import (AsciiType, BytesType, BooleanType,
                           CounterColumnType, DateType, DecimalType,

datastax/python-driver

cassandra/protocol.py
@@ -30,7 +30,7 @@ from cassandra import (Unavailable, WriteTimeout, ReadTimeout,
                        UnsupportedOperation, UserFunctionDescriptor,
                        UserAggregateDescriptor, SchemaTargetType)
 from cassandra.marshal import (int32_pack, int32_unpack, uint16_pack, uint16_unpack,
-                               int8_pack, int8_unpack, uint64_pack, header_pack,
+                               int8_pack, int8_unpack, uint64_pack,
                                v3_header_pack, uint32_pack)
 from cassandra.cqltypes import (AsciiType, BytesType, BooleanType,
                                 CounterColumnType, DateType, DecimalType,