6766d77b72e297a477d98eb257413df1da75fa7b
bjmb <jaume.marhuenda@datastax.com>
2017-04-06 17:12:13 -0400

    Removed unused statements from appveyor, this tests where failing always because the need a cluster to test against

--- appveyor/run_test.ps1
+++ appveyor/run_test.ps1
@@ -20,21 +20,4 @@ nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=unit_results.
 echo "uploading unit results"
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\unit_results.xml))
 
-if($env:ci_type -eq 'standard' -Or $env:ci_type -eq 'long'){
-    echo "Running CQLEngine integration tests"
-    nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=cqlengine_results.xml .\tests\integration\cqlengine
-    $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\cqlengine_results.xml))
-    echo "uploading CQLEngine test results"
-
-    echo "Running standard integration tests"
-    nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=standard_results.xml .\tests\integration\standard
-    $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\standard_results.xml))
-    echo "uploading standard integration test results"
-}
-
-if($env:ci_type -eq 'long'){
-    nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=cqlengine_results.xml .\tests\integration\cqlengine
-    $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\cqlengine_results.xml))
-    echo "uploading standard integration test results"
-}
 exit 0
