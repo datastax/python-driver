1b56cc6930b1f831c6deb69196181da186ecd5fd
bjmb <jaume.marhuenda@datastax.com>
2017-05-08 11:38:40 -0400

    Skipped test for windows

--- tests/integration/standard/test_connection.py
+++ tests/integration/standard/test_connection.py
@@ -32,7 +32,7 @@ from cassandra.connection import Connection
 from cassandra.policies import WhiteListRoundRobinPolicy, HostStateListener
 from cassandra.pool import HostConnectionPool
 
-from tests import is_monkey_patched
+from tests import is_monkey_patched, notwindows
 from tests.integration import use_singledc, PROTOCOL_VERSION, get_node, CASSANDRA_IP, local
 
 try:
@@ -360,6 +360,9 @@ class ConnectionTests(object):
         for t in threads:
             t.join()
 
+    # We skip this one for windows because the clock is not as
+    # granular as in linux
+    @notwindows
     def test_connect_timeout(self):
         # Underlying socket implementations don't always throw a socket timeout even with min float
         # This can be timing sensitive, added retry to ensure failure occurs if it can
