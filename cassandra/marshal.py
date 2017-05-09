7a2937931231268e3d736ca0209911f2e16388f5
Alan Boudreault <alan@alanb.ca>
2017-05-09 17:12:52 -0400

    raise an exception if a number cannot be encoded as vint

--- cassandra/marshal.py
+++ cassandra/marshal.py
@@ -127,8 +127,8 @@ def vints_unpack(term):  # noqa
 def vints_pack(values):
     revbytes = bytearray()
     values = [int(v) for v in values[::-1]]
-    for v in values:
-        v = encode_zig_zag(v)
+    for value in values:
+        v = encode_zig_zag(value)
         if v < 128:
             revbytes.append(v)
         else:
@@ -144,6 +144,9 @@ def vints_pack(values):
                 reserved_bits = min(num_extra_bytes + 1, 8)
                 revbytes.append(v & 0xff)
                 v >>= 8
+
+            if num_extra_bytes > 8:
+                raise ValueError('Value %d is too big and cannot be encoded as vint' % value)
 
             # We can now store the last bits in the first byte
             n = 8 - num_extra_bytes
