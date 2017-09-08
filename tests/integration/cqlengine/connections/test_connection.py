e71f4b4b4abc47fb7b1ee31628a24690d3e0ea5b
bjmb <jaumemarhuenda@gmail.com>
2017-09-08 11:03:35 -0400

    Refactor cqlengine.connections.test_conection.py

--- tests/integration/cqlengine/connections/test_connection.py
+++ tests/integration/cqlengine/connections/test_connection.py
@@ -38,65 +38,9 @@ class TestConnectModel(Model):
     keyspace = columns.Text()
 
 
-class ConnectionTest(BaseCassEngTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        connection.unregister_connection('default')
-        cls.keyspace1 = 'ctest1'
-        cls.keyspace2 = 'ctest2'
-        super(ConnectionTest, cls).setUpClass()
-        cls.setup_cluster = Cluster(protocol_version=PROTOCOL_VERSION)
-        cls.setup_session = cls.setup_cluster.connect()
-        ddl = "CREATE KEYSPACE {0} WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': '{1}'}}".format(cls.keyspace1, 1)
-        execute_with_long_wait_retry(cls.setup_session, ddl)
-        ddl = "CREATE KEYSPACE {0} WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': '{1}'}}".format(cls.keyspace2, 1)
-        execute_with_long_wait_retry(cls.setup_session, ddl)
-
-    @classmethod
-    def tearDownClass(cls):
-        execute_with_long_wait_retry(cls.setup_session, "DROP KEYSPACE {0}".format(cls.keyspace1))
-        execute_with_long_wait_retry(cls.setup_session, "DROP KEYSPACE {0}".format(cls.keyspace2))
-        models.DEFAULT_KEYSPACE = DEFAULT_KEYSPACE
-        cls.setup_cluster.shutdown()
-        setup_connection(DEFAULT_KEYSPACE)
-        models.DEFAULT_KEYSPACE
-
-    def setUp(self):
-        self.c = Cluster(protocol_version=PROTOCOL_VERSION)
-        self.session1 = self.c.connect(keyspace=self.keyspace1)
-        self.session1.row_factory = dict_factory
-        self.session2 = self.c.connect(keyspace=self.keyspace2)
-        self.session2.row_factory = dict_factory
-
+class ConnectionTest(unittest.TestCase):
     def tearDown(self):
-        self.c.shutdown()
         connection.unregister_connection("default")
-
-    def test_connection_session_switch(self):
-        """
-        Test to ensure that when the default keyspace is changed in a session and that session,
-        is set in the connection class, that the new defaul keyspace is honored.
-
-        @since 3.1
-        @jira_ticket PYTHON-486
-        @expected_result CQLENGINE adopts whatever keyspace is passed in vai the set_session method as default
-
-        @test_category object_mapper
-        """
-
-        connection.set_session(self.session1)
-        sync_table(TestConnectModel)
-        TCM1 = TestConnectModel.create(id=1, keyspace=self.keyspace1)
-        connection.set_session(self.session2)
-        sync_table(TestConnectModel)
-        TCM2 = TestConnectModel.create(id=1, keyspace=self.keyspace2)
-        connection.set_session(self.session1)
-        self.assertEqual(1, TestConnectModel.objects.count())
-        self.assertEqual(TestConnectModel.objects.first(), TCM1)
-        connection.set_session(self.session2)
-        self.assertEqual(1, TestConnectModel.objects.count())
-        self.assertEqual(TestConnectModel.objects.first(), TCM2)
 
     @local
     def test_connection_setup_with_setup(self):
@@ -123,3 +67,63 @@ class ConnectionTest(BaseCassEngTestCase):
         connection.default()
         number_of_clusters_after = len(_clusters_for_shutdown)
         self.assertEqual(number_of_clusters_after - number_of_clusters_before, 1)
+
+
+class SeveralConnectionsTest(BaseCassEngTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        connection.unregister_connection('default')
+        cls.keyspace1 = 'ctest1'
+        cls.keyspace2 = 'ctest2'
+        super(SeveralConnectionsTest, cls).setUpClass()
+        cls.setup_cluster = Cluster(protocol_version=PROTOCOL_VERSION)
+        cls.setup_session = cls.setup_cluster.connect()
+        ddl = "CREATE KEYSPACE {0} WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': '{1}'}}".format(cls.keyspace1, 1)
+        execute_with_long_wait_retry(cls.setup_session, ddl)
+        ddl = "CREATE KEYSPACE {0} WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': '{1}'}}".format(cls.keyspace2, 1)
+        execute_with_long_wait_retry(cls.setup_session, ddl)
+
+    @classmethod
+    def tearDownClass(cls):
+        execute_with_long_wait_retry(cls.setup_session, "DROP KEYSPACE {0}".format(cls.keyspace1))
+        execute_with_long_wait_retry(cls.setup_session, "DROP KEYSPACE {0}".format(cls.keyspace2))
+        models.DEFAULT_KEYSPACE = DEFAULT_KEYSPACE
+        cls.setup_cluster.shutdown()
+        setup_connection(DEFAULT_KEYSPACE)
+        models.DEFAULT_KEYSPACE
+
+    def setUp(self):
+        self.c = Cluster(protocol_version=PROTOCOL_VERSION)
+        self.session1 = self.c.connect(keyspace=self.keyspace1)
+        self.session1.row_factory = dict_factory
+        self.session2 = self.c.connect(keyspace=self.keyspace2)
+        self.session2.row_factory = dict_factory
+
+    def tearDown(self):
+        self.c.shutdown()
+
+    def test_connection_session_switch(self):
+        """
+        Test to ensure that when the default keyspace is changed in a session and that session,
+        is set in the connection class, that the new defaul keyspace is honored.
+
+        @since 3.1
+        @jira_ticket PYTHON-486
+        @expected_result CQLENGINE adopts whatever keyspace is passed in vai the set_session method as default
+
+        @test_category object_mapper
+        """
+
+        connection.set_session(self.session1)
+        sync_table(TestConnectModel)
+        TCM1 = TestConnectModel.create(id=1, keyspace=self.keyspace1)
+        connection.set_session(self.session2)
+        sync_table(TestConnectModel)
+        TCM2 = TestConnectModel.create(id=1, keyspace=self.keyspace2)
+        connection.set_session(self.session1)
+        self.assertEqual(1, TestConnectModel.objects.count())
+        self.assertEqual(TestConnectModel.objects.first(), TCM1)
+        connection.set_session(self.session2)
+        self.assertEqual(1, TestConnectModel.objects.count())
+        self.assertEqual(TestConnectModel.objects.first(), TCM2)
