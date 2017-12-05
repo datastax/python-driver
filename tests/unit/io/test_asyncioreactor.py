17957cdb6425e9a693291d1ad252a9050791b0bc
Jim Witschey <jim.witschey@gmail.com>
2017-12-05 17:05:21 -0500

    add asyncio timer tests

--- tests/unit/io/test_asyncioreactor.py
+++ tests/unit/io/test_asyncioreactor.py
@@ -1,11 +1,14 @@
 from cassandra.io.asyncioreactor import AsyncioConnection
 from tests import is_monkey_patched
-from tests.unit.io.utils import TimerConnectionTests
+from tests.unit.io.utils import TimerCallback, TimerTestMixin
+
+from mock import patch
 
 import unittest
+import time
 
 
-class AsyncioTimerConnectionTests(unittest.TestCase, TimerConnectionTests):
+class AsyncioTimerTests(TimerTestMixin, unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -14,6 +17,28 @@ class AsyncioTimerConnectionTests(unittest.TestCase, TimerConnectionTests):
         cls.connection_class = AsyncioConnection
         AsyncioConnection.initialize_reactor()
 
+    @property
+    def create_timer(self):
+        return self.connection.create_timer
+
+    @property
+    def _timers(self):
+        raise RuntimeError('no TimerManager for AsyncioConnection')
+
+    def setUp(self):
+        socket_patcher = patch('socket.socket')
+        self.addCleanup(socket_patcher.stop)
+        socket_patcher.start()
+
+        super(AsyncioTimerTests, self).setUp()
+
     def test_timer_cancellation(self):
-        pass
-    test_timer_cancellation.__test__ = False
+        # Various lists for tracking callback stage
+        timeout = .1
+        callback = TimerCallback(timeout)
+        timer = self.create_timer(timeout, callback.invoke)
+        timer.cancel()
+        # Release context allow for timer thread to run.
+        time.sleep(.2)
+        # Assert that the cancellation was honored
+        self.assertFalse(callback.was_invoked())
