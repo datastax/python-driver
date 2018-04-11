c2fc640d4d2d9f06c31130237e64811a6afab051
Jaume Marhuenda <jaumemarhuenda@gmail.com>
2018-04-11 17:36:28 -0400

    PYTHON-968 Support IS NOT NULL operator in cqlengine

--- tests/integration/cqlengine/operators/test_where_operators.py
+++ tests/integration/cqlengine/operators/test_where_operators.py
@@ -12,30 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest import TestCase
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest  # noqa
+
 from cassandra.cqlengine.operators import *
+
+from uuid import uuid4
+
+from cassandra.cqlengine.management import sync_table, drop_table
+from cassandra.cqlengine.operators import IsNotNullOperator
+from cassandra.cqlengine.statements import IsNotNull
+from cassandra import InvalidRequest
+
+from tests.integration.cqlengine.base import TestQueryUpdateModel, BaseCassEngTestCase
+from tests.integration.cqlengine.operators import check_lookup
 
 import six
 
 
-class TestWhereOperators(TestCase):
+class TestWhereOperators(unittest.TestCase):
 
     def test_symbol_lookup(self):
         """ tests where symbols are looked up properly """
 
-        def check_lookup(symbol, expected):
-            op = BaseWhereOperator.get_operator(symbol)
-            self.assertEqual(op, expected)
-
-        check_lookup('EQ', EqualsOperator)
-        check_lookup('NE', NotEqualsOperator)
-        check_lookup('IN', InOperator)
-        check_lookup('GT', GreaterThanOperator)
-        check_lookup('GTE', GreaterThanOrEqualOperator)
-        check_lookup('LT', LessThanOperator)
-        check_lookup('LTE', LessThanOrEqualOperator)
-        check_lookup('CONTAINS', ContainsOperator)
-        check_lookup('LIKE', LikeOperator)
+        check_lookup(self, 'EQ', EqualsOperator)
+        check_lookup(self, 'NE', NotEqualsOperator)
+        check_lookup(self, 'IN', InOperator)
+        check_lookup(self, 'GT', GreaterThanOperator)
+        check_lookup(self, 'GTE', GreaterThanOrEqualOperator)
+        check_lookup(self, 'LT', LessThanOperator)
+        check_lookup(self, 'LTE', LessThanOrEqualOperator)
+        check_lookup(self, 'CONTAINS', ContainsOperator)
+        check_lookup(self, 'LIKE', LikeOperator)
 
     def test_operator_rendering(self):
         """ tests symbols are rendered properly """
@@ -48,3 +58,55 @@ class TestWhereOperators(TestCase):
         self.assertEqual("<=", six.text_type(LessThanOrEqualOperator()))
         self.assertEqual("CONTAINS", six.text_type(ContainsOperator()))
         self.assertEqual("LIKE", six.text_type(LikeOperator()))
+
+
+class TestIsNotNull(BaseCassEngTestCase):
+    def test_is_not_nul_to_cql(self):
+        """
+        Verify that IsNotNull is converted correctly to CQL
+
+        @since 2.5
+        @jira_ticket PYTHON-968
+        @expected_result the strings match
+
+        @test_category cqlengine
+        """
+
+        check_lookup(self, 'IS NOT NULL', IsNotNullOperator)
+
+        # The * is not expanded because there are no referred fields
+        self.assertEqual(
+            str(TestQueryUpdateModel.filter(IsNotNull("text")).limit(2)),
+            "SELECT * FROM cqlengine_test.test_query_update_model WHERE \"text\" IS NOT NULL LIMIT 2"
+        )
+
+        # We already know partition so cqlengine doesn't query for it
+        self.assertEqual(
+            str(TestQueryUpdateModel.filter(IsNotNull("text"), partition=uuid4())),
+            "SELECT \"cluster\", \"count\", \"text\", \"text_set\", "
+            "\"text_list\", \"text_map\" FROM cqlengine_test.test_query_update_model "
+            "WHERE \"text\" IS NOT NULL AND \"partition\" = %(0)s LIMIT 10000"
+        )
+
+    def test_is_not_null_execution(self):
+        """
+        Verify that CQL statements have correct syntax when executed
+        If we wanted them to return something meaningful and not a InvalidRequest
+        we'd have to create an index in search for the column we are using
+        IsNotNull
+
+        @since 2.5
+        @jira_ticket PYTHON-968
+        @expected_result InvalidRequest is arisen
+
+        @test_category cqlengine
+        """
+        sync_table(TestQueryUpdateModel)
+        self.addCleanup(drop_table, TestQueryUpdateModel)
+
+        # Raises InvalidRequest instead of dse.protocol.SyntaxException
+        with self.assertRaises(InvalidRequest):
+            list(TestQueryUpdateModel.filter(IsNotNull("text")))
+
+        with self.assertRaises(InvalidRequest):
+            list(TestQueryUpdateModel.filter(IsNotNull("text"), partition=uuid4()))
