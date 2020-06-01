805869d5475601a0dfcc96893dadbf20c031e0e9
dgarcia360 <dgarcia360@outlook.com>
2020-06-01 10:19:42 +0100

    Fixed duplicated comment

--- docs/conf.py
+++ docs/conf.py
@@ -101,10 +101,6 @@ html_theme = 'sphinx_scylladb_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
 html_theme_options = {
     'header_links': [
     ('Scylla Cloud', 'https://docs.scylladb.com/scylla-cloud/'),
