548864395cd9076f5cee416c4b562436d4ed63df
Tzach Livyatan <tzach@scylladb.com>
2020-06-03 22:22:43 +0300

    Add Scylla Python Driver to the top menu

--- docs/conf.py
+++ docs/conf.py
@@ -103,6 +103,7 @@ html_theme = 'sphinx_scylladb_theme'
 # documentation.
 html_theme_options = {
     'header_links': [
+    ('Scylla Python Driver', '/'),
     ('Scylla Cloud', 'https://docs.scylladb.com/scylla-cloud/'),
     ('Scylla University', 'https://university.scylladb.com/'),
     ('ScyllaDB Home', 'https://www.scylladb.com/')],
