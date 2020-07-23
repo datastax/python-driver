0390dcf86fdb964960289a90691614359e35a098
Israel Fruchter <fruch@scylladb.com>
2020-07-23 14:26:19 +0300

    Release 3.22.3
    * 3ea763 fddaa7 - intergation tests running with scylla 4.1 on each PR
    * b740a3 - new wheels for python3.9 (only for mac and linux, for now)

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -22,7 +22,7 @@ class NullHandler(logging.Handler):
 
 logging.getLogger('cassandra').addHandler(NullHandler())
 
-__version_info__ = (3, 22, 2)
+__version_info__ = (3, 22, 3)
 __version__ = '.'.join(map(str, __version_info__))
 
 
