0a93db4deb35514c876041b97bd169a0ad26a5dd
dgarcia360 <dgarcia360@outlook.com>
2020-08-05 18:42:56 +0200

    Change latest to version

--- docs/_utils/deploy.sh
+++ docs/_utils/deploy.sh
@@ -3,8 +3,8 @@
 # Clone repo
 git clone "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" --branch gh-pages --single-branch gh-pages
 cp -r docs/_build/dirhtml/* gh-pages
-# Redirect index to master
-cp docs/_utils/redirect.html gh-pages/index.html
+# Redirect index to latest version
+./docs/_utils/redirect.sh > gh-pages/index.html
 # Deploy
 cd gh-pages
 touch .nojekyll
--- docs/conf.py
+++ docs/conf.py
@@ -231,7 +231,7 @@ man_pages = [
 # Whitelist pattern for tags (set to None to ignore all tags)
 smv_tag_whitelist = r'\b(3.22.0-scylla|3.21.0-scylla)\b'
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r"^master$"
+smv_branch_whitelist = r"None"
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = r"^origin$"
 # Pattern for released versions
