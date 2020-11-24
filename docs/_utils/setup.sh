8722ed6284435d58fd2f879e2e3f52c2fd69a657
David Garcia <dgarcia360@outlook.com>
2020-11-24 18:57:38 +0000

    docs:update theme when there is a non-breaking release (#76)
    docs: deploy now deletes previous doc builds
    
    Remove conf.py.save
    
    Update python dependency
    
    Update python

--- docs/_utils/setup.sh
+++ docs/_utils/setup.sh
@@ -8,3 +8,4 @@ fi
 which python3 || { echo "Failed to find python3. Try installing Python for your operative system: https://www.python.org/downloads/" && exit 1; }
 which poetry || curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/1.1.3/get-poetry.py | python3 - && source ${HOME}/.poetry/env
 poetry install
+poetry update
