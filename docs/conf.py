b63d590f8827b5afd63a5c8269fb43b39909d065
Israel Fruchter <fruch@scylladb.com>
2020-12-20 19:58:07 +0200

    docs: Add all new branchs to docs

--- docs/conf.py
+++ docs/conf.py
@@ -112,7 +112,7 @@ redirects_file = "_utils/redirections.yaml"
 
 # -- Options for multiversion --------------------------------------------------
 # Whitelist pattern for tags (set to None to ignore all tags)
-smv_tag_whitelist = r'\b(3.22.0-scylla|3.21.0-scylla)\b'
+smv_tag_whitelist = r'\b(3.22.0-scylla|3.21.0-scylla|3.22.3-scylla|3.24.0-scylla)\b'
 # Whitelist pattern for branches (set to None to ignore all branches)
 smv_branch_whitelist = "None"
 # Whitelist pattern for remotes (set to None to use local branches only)
