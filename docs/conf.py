ec1b46fc72a4a5448d245fbe412711404cf482ab
dgarcia360 <dgarcia360@outlook.com>
2020-12-31 17:07:38 +0100

    docs: add missing extension

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ from recommonmark.transform import AutoStructify
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_scylladb_theme', 'sphinx_multiversion', 'recommonmark']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages', 'sphinx.ext.viewcode', 'sphinx_scylladb_theme', 'sphinx_multiversion', 'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
