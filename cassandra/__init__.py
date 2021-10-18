f3753a7e307602d4338e4ab656dcaeac20d9cfda
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2021-10-18 22:52:04 +0300

    Bump package version

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -22,7 +22,7 @@ class NullHandler(logging.Handler):
 
 logging.getLogger('cassandra').addHandler(NullHandler())
 
-__version_info__ = (3, 24, 5)
+__version_info__ = (3, 24, 6)
 __version__ = '.'.join(map(str, __version_info__))
 
 
