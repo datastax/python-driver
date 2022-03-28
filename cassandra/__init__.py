66046ded8e20006adc968b3d7d32d025e4eab679
Israel Fruchter <fruch@scylladb.com>
2022-03-28 18:45:26 +0300

    Release 3.25.3

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -22,7 +22,7 @@ class NullHandler(logging.Handler):
 
 logging.getLogger('cassandra').addHandler(NullHandler())
 
-__version_info__ = (3, 25, 2)
+__version_info__ = (3, 25, 3)
 __version__ = '.'.join(map(str, __version_info__))
 
 
