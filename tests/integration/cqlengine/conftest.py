bf5db5158e13467092193b345d419c2b327fc6d1
Karol Baryła <karol.baryla@scylladb.com>
2023-01-27 12:44:34 +0100

    tests/integration/cqlengine: move from nose to pytest
    We use pytest to run our tests, but cqlenginge inegration tests were
    using nose's setup_package / teardown_package functions which are
    not supported by pytest. This causes most of those tests to fail.
    
    This commit changes this to pytest's autouse fixture, increasing amount
    of passing tests in integration/cqlengine from 185 to 516.

---
+++ tests/integration/cqlengine/conftest.py
@@ -0,0 +1,54 @@
+# Copyright ScyllaDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copyright DataStax, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import warnings
+import os
+
+import pytest
+
+from cassandra.cqlengine import connection
+from cassandra.cqlengine.management import create_keyspace_simple, drop_keyspace, CQLENG_ALLOW_SCHEMA_MANAGEMENT
+from tests.integration import use_single_node
+
+from . import setup_connection, DEFAULT_KEYSPACE
+
+
+@pytest.fixture(scope='package', autouse=True)
+def cqlengine_fixture():
+    warnings.simplefilter('always')  # for testing warnings, make sure all are let through
+    os.environ[CQLENG_ALLOW_SCHEMA_MANAGEMENT] = '1'
+
+    use_single_node()
+
+    setup_connection(DEFAULT_KEYSPACE)
+    create_keyspace_simple(DEFAULT_KEYSPACE, 1)
+
+    yield
+
+    drop_keyspace(DEFAULT_KEYSPACE)
+    connection.unregister_connection("default")
