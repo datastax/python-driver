4bf660e30831d9a4702542ccb8a76bcce9691ecf
weideng1 <weideng1@users.noreply.github.com>
2023-05-04 22:10:54 -0600

    force cqlengine test to clean up ccm cluster

--- tests/integration/cqlengine/conftest.py
+++ tests/integration/cqlengine/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from tests.integration import teardown_package as parent_teardown_package
 from tests.integration.cqlengine import setup_package, teardown_package
 
 @pytest.fixture(scope='session', autouse=True)
@@ -7,3 +8,4 @@ def setup_and_teardown_packages():
     setup_package()
     yield
     teardown_package()
+    parent_teardown_package()
