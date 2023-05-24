b400697b71b4f51b05e99b0b3e844c64a989a0fe
Bret McGuire <bret.mcguire@datastax.com>
2023-05-24 12:06:18 -0500

    Revert "remove unnecessary import __future__ (#1156)"
    This reverts commit 6894b028ffda01a63fad1deebe0b5300349d1611.

--- cassandra/connection.py
+++ cassandra/connection.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import  # to enable import io from stdlib
 from collections import defaultdict, deque
 import errno
 from functools import wraps, partial, total_ordering
--- cassandra/protocol.py
+++ cassandra/protocol.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import  # to enable import io from stdlib
 from collections import namedtuple
 import logging
 import socket
