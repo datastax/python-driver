1e31d1d7d420694532d5c183dd8a30080a261398
Bret McGuire <bret.mcguire@datastax.com>
2023-05-30 17:44:39 -0500

    No really, I'd like the new package to be in the package

--- setup.py
+++ setup.py
@@ -424,7 +424,8 @@ def run_setup(extensions):
         packages=[
             'cassandra', 'cassandra.io', 'cassandra.cqlengine', 'cassandra.graph',
             'cassandra.datastax', 'cassandra.datastax.insights', 'cassandra.datastax.graph',
-            'cassandra.datastax.graph.fluent', 'cassandra.datastax.cloud'
+            'cassandra.datastax.graph.fluent', 'cassandra.datastax.cloud',
+            "cassandra.column_encryption"
         ],
         keywords='cassandra,cql,orm,dse,graph',
         include_package_data=True,
