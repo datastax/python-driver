2db3728a4791ef08e41bdd2631b34ade0cd2dd8b
Bret McGuire <absurdfarce@users.noreply.github.com>
2023-06-05 09:36:50 -0500

    PYTHON-1352 Add vector type, codec + support for parsing CQL type (#1161)

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -22,7 +22,7 @@ class NullHandler(logging.Handler):
 
 logging.getLogger('cassandra').addHandler(NullHandler())
 
-__version_info__ = (3, 27, 0)
+__version_info__ = (3, 28, 0b1)
 __version__ = '.'.join(map(str, __version_info__))
 
 
