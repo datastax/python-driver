73b86ec96a0b9ab488316963d141f00be54e19ad
sylwiaszunejko <sylwia.szunejko@gmail.com>
2023-08-16 12:34:00 +0200

    Add test for rate limit exceeded

---
+++ tests/unit/test_protocol_features.py
@@ -0,0 +1,27 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest  # noqa
+
+import logging
+
+from cassandra.protocol_features import ProtocolFeatures
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TestProtocolFeatures(unittest.TestCase):
+    def test_parsing_rate_limit_error(self):
+        """
+        Testing the parsing of the options command
+        """
+        class OptionsHolder(object):
+            options = {
+                'SCYLLA_RATE_LIMIT_ERROR': ["ERROR_CODE=123"]
+            }
+
+        protocol_features = ProtocolFeatures.parse_from_supported(OptionsHolder().options)
+
+        self.assertEqual(protocol_features.rate_limit_error, 123)
+        self.assertEqual(protocol_features.shard_id, 0)
+        self.assertEqual(protocol_features.sharding_info, None)
