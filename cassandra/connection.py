72978f534213970a67eabb51cf5b9e1c420540bf
Lukasz Antoniak <Lukasz.Antoniak@gmail.com>
2025-05-21 20:17:55 +0200

    Revert "PYTHON-1331 Recreate expired SSL certificates for integration tests"
    This reverts commit 9698893c96179ea58d1d5f6f50aa09b234219130.

--- cassandra/connection.py
+++ cassandra/connection.py
@@ -790,13 +790,15 @@ class Connection(object):
             self.ssl_options.update(self.endpoint.ssl_options or {})
         elif self.endpoint.ssl_options:
             self.ssl_options = self.endpoint.ssl_options
-        self._check_hostname = self.ssl_options.get('check_hostname', False)
 
         # PYTHON-1331
         #
         # We always use SSLContext.wrap_socket() now but legacy configs may have other params that were passed to ssl.wrap_socket()...
         # and either could have 'check_hostname'.  Remove these params into a separate map and use them to build an SSLContext if
         # we need to do so.
+        #
+        # Note the use of pop() here; we are very deliberately removing these params from ssl_options if they're present.  After this
+        # operation ssl_options should contain only args needed for the ssl_context.wrap_socket() call.
         if not self.ssl_context and self.ssl_options:
             self.ssl_context = self._build_ssl_context_from_options()
 
