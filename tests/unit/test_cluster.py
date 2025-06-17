4956254a07041b2e55aea0d2a979b0ea98574dc2
Lukasz Antoniak <Lukasz.Antoniak@gmail.com>
2025-06-17 17:05:24 +0200

    Unit tests consolidation

--- tests/unit/test_cluster.py
+++ tests/unit/test_cluster.py
@@ -129,47 +129,6 @@ class ClusterTest(unittest.TestCase):
         for n in (0, mn, 128):
             self.assertRaises(ValueError, c.set_max_requests_per_connection, d, n)
 
-    # Tests verifies that driver can connect to SNI endpoint even when one IP
-    # returned by the DNS resolution of SNI raises error. Mocked SNI resolution method
-    # returns two IPs. Trying to connect to the first one always fails
-    # with socket exception.
-    def test_sni_round_robin_dns_resolution(self):
-        def _mocked_cloud_config(cloud_config, create_pyopenssl_context):
-            config = CloudConfig.from_dict({})
-            config.sni_host = 'proxy.datastax.com'
-            config.sni_port = 9042
-            # for 2e25021d-8d72-41a7-a247-3da85c5d92d2 we return IP 127.0.0.1 to which connection fails
-            config.host_ids = ['2e25021d-8d72-41a7-a247-3da85c5d92d2', '8c4b6ed7-f505-4226-b7a4-41f322520c1f']
-            return config
-
-        def _mocked_proxy_dns_resolution(self):
-            return [
-                (socket.AF_UNIX, socket.SOCK_STREAM, 0, None, ('127.0.0.1', 9042)),
-                (socket.AF_UNIX, socket.SOCK_STREAM, 0, None, ('127.0.0.2', 9042))
-            ]
-
-        def _mocked_try_connect(self, host):
-            address, port = host.endpoint.resolve()
-            if address == '127.0.0.1':
-                raise socket.error
-            return MagicMock(spec=Connection)
-
-        with patch('cassandra.datastax.cloud.get_cloud_config', _mocked_cloud_config):
-            with patch.object(SniEndPoint, '_resolve_proxy_addresses', _mocked_proxy_dns_resolution):
-                cloud_config = {
-                    'secure_connect_bundle': '/path/to/secure-connect-dbname.zip'
-                }
-                cluster = Cluster(cloud=cloud_config)
-                lbp = MockOrderedPolicy()
-                cluster.load_balancing_policy = lbp
-                with patch.object(ControlConnection, '_try_connect', _mocked_try_connect):
-                    for endpoint in cluster.endpoints_resolved:
-                        host, new = cluster.add_host(endpoint, signal=False)
-                        lbp.all_hosts.add(host)
-                    # No NoHostAvailable indicates that test passed.
-                    cluster.control_connection.connect()
-                cluster.shutdown()
-
     # Validate that at least the default LBP can create a query plan with end points that resolve
     # to different addresses initially. This may not be exactly how things play out in practice
     # (the control connection will muck with this even if nothing else does) but it should be
@@ -187,6 +146,7 @@ class ClusterTest(unittest.TestCase):
             lbp.on_up(host)
         with patch.object(SniEndPoint, '_resolve_proxy_addresses', _mocked_proxy_dns_resolution):
             addrs = [host.endpoint.resolve() for host in lbp.make_query_plan()]
+            # single SNI endpoint should be resolved to multiple unique IP addresses
             self.assertEqual(len(addrs), len(set(addrs)))
 
 
