ddd16210e48a58fc493e2aebbe235adc3ad9aa76
Luke Ratcliffe <luke.ratcliffe@trellix.com>
2026-04-23 08:46:51 +0000

    Mark non-security md5 usage to allow for compatibility with fips environments

--- cassandra/metadata.py
+++ cassandra/metadata.py
@@ -19,7 +19,7 @@ from bisect import bisect_left
 from collections import defaultdict
 from collections.abc import Mapping
 from functools import total_ordering
-from hashlib import md5
+import hashlib
 import json
 import logging
 import re
@@ -1864,7 +1864,7 @@ class MD5Token(HashToken):
     def hash_fn(cls, key):
         if isinstance(key, str):
             key = key.encode('UTF-8')
-        return abs(varint_unpack(md5(key).digest()))
+        return abs(varint_unpack(hashlib.md5(key,usedforsecurity=False).digest()))
 
 
 class BytesToken(Token):
